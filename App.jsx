64cae3316e79394b1b149a3083f9c98e2f8306d1
Pedro Vilaca <up202404967@up.pt>
2026-04-20 16:42:29 +0100

    profile status setup implemented

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ProfileSetupScreen from './src/screens/ProfileSetupScreen';
 import WorkoutLogger from './src/screens/WorkoutLogger';
 /* import WorkoutHistory from './src/screens/WorkoutHistory'; */
 
@@ -10,13 +11,14 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="WorkoutLogger"
+        initialRouteName="ProfileSetup"
         screenOptions={{
           headerShown: false,
           contentStyle: { backgroundColor: '#0A0A0A' },
           animation: 'slide_from_right',
         }}
       >
+        <Stack.Screen name="ProfileSetup" component={ProfileSetupScreen} />
         <Stack.Screen name="WorkoutLogger" component={WorkoutLogger} />
       </Stack.Navigator>
     </NavigationContainer>
