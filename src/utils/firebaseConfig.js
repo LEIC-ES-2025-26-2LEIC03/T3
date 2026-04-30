9e97a7078a5c30ac37ea09e3bcf127484a131258
Filipe Carvalho Cruz <up202404158@up.pt>
2026-04-30 22:08:40 +0100

    feature completed

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -25,13 +25,13 @@ import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBENB5lem4FOlHbXaD8WGfYx4gp0YE0b60",
-  authDomain: "w8-db-ee67f.firebaseapp.com",
-  projectId: "w8-db-ee67f",
-  storageBucket: "w8-db-ee67f.firebasestorage.app",
-  messagingSenderId: "242552717566",
-  appId: "1:242552717566:web:9a3886e63aa60b9e1165af",
-  measurementId: "G-36BWGY2CXZ"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Prevent duplicate initialization in hot-reload / fast-refresh environments
